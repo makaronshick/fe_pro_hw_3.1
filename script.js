b855c08233c4604a390559ae4ac243ef0f181aa9
makaronshick <makaronshick@ukr.net>
2024-07-11 17:34:33 +0300

    unused code deleted

--- script.js
+++ script.js
@@ -7,17 +7,11 @@
 
 console.log('12345 => ', typeof 12345);
 
-console.log('123.45 => ', typeof 123.45);
-
 console.log('some text => ', typeof 'some text');
 
 console.log('true => ', typeof Boolean(1));
 
-console.log('false => ', typeof Boolean(0));
-
 console.log('null => ', typeof null);
-
-console.log('array [1, 2] => ', typeof [1, 2]);
 
 console.log('{object} => ', typeof {prop1:'prop1'});
 
@@ -28,12 +22,3 @@ console.log('Symbol => ', typeof Symbol());
 console.log('function console.log => ', typeof console.log);
 
 console.log('const without value => ', typeof someConst);
-
-const someArray = [1, 2];
-console.log('non-existent array index => ', typeof someArray[2]);
-
-console.log('string / null => ', typeof 'string' / null);
-
-console.log('null * 100 => ', typeof null * 100);
-
-console.log('number 99999 to the power of 99 => ',  Math.pow(99999, 99));
